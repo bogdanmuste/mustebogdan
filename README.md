93d1e4cb06b421ad6668103163b803cb92db3fa8
bogdanmuste <bogdanmuste1@gmail.com>
2021-02-11 11:54:23 -0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# mustebogdan
+
+
+## TODO
+
+[x] inprove menu clicks link.innerHTML. toLowerCase;
+[x] fetch skills
+[x] show differit favorite skills
+[x] sort by nr of endormens
---
+++ fuctions.js
@@ -0,0 +1,66 @@
+function hide(id) {
+    var el = document.getElementById(id);
+    el.style.display = 'none';
+}
+
+function show(id) {
+    document.getElementById(id).style.display = '';
+}
+
+function hideAllPages() {
+    var pages = Array.from(document.querySelectorAll(".page"));
+    pages.forEach(function(page){
+        hide(page.id);
+    });
+
+}
+function showPage(pageId) {
+    hideAllPages();
+    show(pageId);
+}
+
+function listenMenuClicks() {
+    document.addEventListener("click", function (e) {
+        var link = e.target;
+        if (link.matches("#top-menu-bar a")) {
+            var id = link.getAttribute("data-id");
+            showPage(id);
+
+        }
+
+    });
+}
+
+listenMenuClicks();
+
+showPage("skills");
+
+var allSkills =  [];
+
+function showSkills(skills) {
+
+    var allSkillsHtml = skills.map(function(skill){
+        var cls = skill.favorite ? "favorite-skill" : "";
+        return `<li class="${cls}">
+        ${skill.name} <span>(${skill.endorsements}) </li>`;
+    });
+    
+    
+    var SkillsEl = document.querySelector("#skills ul");
+    SkillsEl.innerHTML = allSkillsHtml.join("");
+}
+
+fetch("skills.json")
+    .then(function(r) {
+        return r.json();
+    })
+    .then(function (skills){
+        skills.sort(function(s1, s2){
+            return s2.endorsements - s1.endorsements;
+        })
+        showSkills(skills)
+    });
+
+
+//showSkills(allSkills);
+
