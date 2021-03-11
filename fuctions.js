5b5b83a961b03373510e8db6d69d28b20952e931
bogdanmuste <77117160+bogdanmuste@users.noreply.github.com>
2021-03-10 23:38:05 -0800

    11.03.

--- fuctions.js
+++ fuctions.js
@@ -61,4 +61,3 @@ fetch("skills.json")
 
 //showSkills(allSkills);
 
-
---
+++ homework.js
@@ -0,0 +1,12 @@
+
+
+function getAvrage(employees) {
+    employees.forEach(employee => {
+        console.info(employee.salary);
+        sum = sum + employee.salary;
+    });
+    return 1;
+}
+const average = getAvrage(employees);
+console average = getAvrage(employees);
+console.info(average);
